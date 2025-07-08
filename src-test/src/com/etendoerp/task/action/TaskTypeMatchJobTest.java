2b6503d27f849f6ff3affd8215f90b500f3eae7f
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2025-07-08 09:40:53 -0300

    Feature ETP-1713: Change apache common lang 3

--- src-test/src/com/etendoerp/task/action/TaskTypeMatchJobTest.java
+++ src-test/src/com/etendoerp/task/action/TaskTypeMatchJobTest.java
@@ -30,7 +30,7 @@ import static org.mockito.Mockito.verify;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.mutable.MutableBoolean;
+import org.apache.commons.lang3.mutable.MutableBoolean;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
 import org.junit.jupiter.api.Test;
--- src/com/etendoerp/task/action/TaskTypeMatchJob.java
+++ src/com/etendoerp/task/action/TaskTypeMatchJob.java
@@ -3,8 +3,8 @@ package com.etendoerp.task.action;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.mutable.MutableBoolean;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.mutable.MutableBoolean;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.codehaus.jettison.json.JSONObject;
